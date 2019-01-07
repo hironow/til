1d078d3e571696e66fe1ec8c2ac399396a117347
hironow <hironow365@gmail.com>
2019-01-08 02:48:41 +0900

    fix
    [skip ci] use fresh

--- go/wire/app.go
+++ go/wire/app.go
@@ -29,5 +29,5 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), server))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%s", port), server))
 }
